bc3d6ecdd9b282c5342928879c70199c29e4866d
Andrew Withers <andreww@thomasdev.co.uk>
2016-12-13 15:42:54 +0000

    Add MVC plumbing with dummy service and content pages, including sample contact form

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -3,12 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheWorld.ViewModels;
+using TheWorld.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+
+        public AppController(IMailService mailService, IConfigurationRoot config)
+        {
+            _mailService = mailService;
+            _config = config;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(ContactVM model)
+        {
+
+            if (model.Email.Contains("aol.com"))
+                ModelState.AddModelError(String.Empty, "We don't support AOL addresses");
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "From The World", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent OK";
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
