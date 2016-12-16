66cf6546c66e74991acb18e0bbfed422db50c8b3
Andrew Withers <andreww@thomasdev.co.uk>
2016-12-16 17:37:09 +0000

    Add bootstrap and font-awesome

--- TheWorld/wwwroot/js/site.js
+++ TheWorld/wwwroot/js/site.js
@@ -2,35 +2,17 @@
 (function () {
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
 
     $("#sidebarToggle").on("click", function () {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
 
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.removeClass("fa-angle-left");
+            $icon.addClass("fa-angle-right");
         } else {
-            $(this).text("Hide Sidebar");
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
         }
     });
-
-    /*
-    var $username = $("#username");
-    $username.text("Anders Web");
-
-    var $main = $("#main");
-
-    $main.on("mouseenter", function () {
-        main.style = "background-color: #888";
-    });
-
-    $main.on("mouseleave", function () {
-        main.style = "";
-    });
-
-    var $menuItems = $("ul.menu li a");
-    $menuItems.on("click", function () {
-        var $this = $(this);
-        alert($this.text() + " clicked");
-    });
-    */
 })();
