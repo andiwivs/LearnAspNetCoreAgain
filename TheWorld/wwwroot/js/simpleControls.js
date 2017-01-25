13681ccbb9bb9dbc1b76304c4376809ca788aae3
Andrew Withers <andiwivs@gmail.com>
2017-01-25 00:03:01 +0000

    Add angular with trips module

---
+++ TheWorld/wwwroot/js/simpleControls.js
@@ -0,0 +1,19 @@
+﻿(function () {
+
+    "use strict";
+
+    angular.module("simpleControls", [])
+        .directive("waitCursor", waitCursor);
+    
+    function waitCursor() {
+
+        return {
+            restrict: "E",
+            templateUrl: "/views/waitCursor.html",
+            scope: {
+                show: "=displayWhen" // where "=" denotes attribute, exposed as displayWhen
+            }
+        };
+    }
+
+})();
